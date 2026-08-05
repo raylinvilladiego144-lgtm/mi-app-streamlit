8167b432ff93a921f2354b29acd4cfc5b82dcf40
raylinvilladiego144-lgtm <raylinvilladiego144@gmail.com>
2026-08-05 17:01:17 -0500

    Update base.py

--- base.py
+++ base.py
@@ -6,8 +6,6 @@ Clase base declarativa para SQLAlchemy 2.0.
 
 from sqlalchemy.orm import DeclarativeBase
 
-Base = declarative_base()
-
 class Base(DeclarativeBase):
     """
     Clase base para todos los modelos SQLAlchemy.
